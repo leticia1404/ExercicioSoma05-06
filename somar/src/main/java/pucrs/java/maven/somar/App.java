9ab81dd9fc0a7ae19aa8f4c4fe4e583d4c99b01e
leticia1404 <let.demoura@gmail.com>
2019-06-05 15:21:49 -0300

    #1

---
+++ somar/src/main/java/pucrs/java/maven/somar/App.java
@@ -0,0 +1,13 @@
+package pucrs.java.maven.somar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
